49c5c4d3b66fe82715253520e45aea24eb95bdb6
Giuseppe de Santis <gdesantis@gmail.com>
2021-04-23 17:56:23 +0200

    add msw

--- shell/webpack.config.js
+++ shell/webpack.config.js
@@ -8,7 +8,10 @@ module.exports = {
   mode: "development",
   target: "web",
   devServer: {
-    contentBase: path.join(__dirname, "dist"),
+    contentBase: [
+      path.join(__dirname, "dist"),
+      path.join(__dirname, "public")
+    ],
     port: 3001,
   },
   output: {

218f7998f65d3aa395fe2354f16432be4084995f
Giuseppe de Santis <gdesantis@gmail.com>
2021-05-06 00:30:04 +0200

    retrieve plugin definition from mock API

--- shell/src/App.js
+++ shell/src/App.js
@@ -3,35 +3,19 @@ import request from './request';
 import System from './System';
 
 function App() {
-  const [system, setSystem] = useState(undefined);
+  const [plugins, setPlugins] = useState([]);
 
   useEffect(() => {
     async function fetchConfig() {
       try {
-        const config = await request('/config');
-        console.log({ config });
+        const plugins = await request('/plugins');
+        setPlugins(plugins);
       } catch (err) {
-        console.log(err);
+        setPlugins([]);
       }
     }
     fetchConfig();
   }, []);
-
-  function setApp2() {
-    setSystem({
-      url: 'http://localhost:3002/remoteEntry.js',
-      scope: 'app2',
-      module: './Widget',
-    });
-  }
-
-  function setApp3() {
-    setSystem({
-      url: 'http://localhost:3003/remoteEntry.js',
-      scope: 'app3',
-      module: './Widget',
-    });
-  }
 
   return (
     <div
@@ -40,18 +24,9 @@ function App() {
           '-apple-system, BlinkMacSystemFont, "Segoe UI", Roboto, Helvetica, Arial, sans-serif, "Apple Color Emoji", "Segoe UI Emoji", "Segoe UI Symbol"',
       }}
     >
-      <h1>Dynamic System Host</h1>
-      <h2>Shell</h2>
-      <p>
-        The Dynamic System will take advantage Module Federation{' '}
-        <strong>remotes</strong> and <strong>exposes</strong>. It will no load
-        components that have been loaded already.
-      </p>
-      <button onClick={setApp2}>Load App 2 Widget</button>
-      <button onClick={setApp3}>Load App 3 Widget</button>
-      <div style={{ marginTop: '2em' }}>
-        <System system={system} />
-      </div>
+      <h1>Dynamic Plugin System</h1>
+      
+      {plugins.map((plugin) => (<System key={plugin.scope} system={plugin} />))}
     </div>
   );
 }
--- shell/src/mocks/handlers.js
+++ shell/src/mocks/handlers.js
@@ -1,12 +1,21 @@
 import { rest } from 'msw';
 
 export const handlers = [
-  rest.get('/config', (req, res, ctx) => {
+  rest.get('/plugins', (req, res, ctx) => {
     return res(
       ctx.status(200),
-      ctx.json({
-        username: 'admin',
-      })
+      ctx.json([
+        {
+          url: 'http://localhost:3002/remoteEntry.js',
+          scope: 'app2',
+          module: './Widget',          
+        },
+        {
+          url: 'http://localhost:3003/remoteEntry.js',
+          scope: 'app3',
+          module: './Widget',          
+        },        
+      ])
     );
   }),
 ];

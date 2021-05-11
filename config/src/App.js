218ffb3f8ee990e1cd9f5bc9319096dfe97d4002
Giuseppe de Santis <gdesantis@gmail.com>
2021-05-11 08:10:36 +0200

    refactor to function

--- config/src/App.js
+++ config/src/App.js
@@ -1,25 +1,14 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-export default class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {app2: false, app3: false};
+export default function App() {
+  const [plugins, setPlugins] = useState({});
 
-    this.handleChange = this.handleChange.bind(this);
-  }
+  const handleChange = (event) => setPlugins({...plugins, [event.target.name]: event.target.checked});
 
-  handleChange(event) {
-    this.setState({...this.state, [event.target.name]: event.target.checked}, () => {
-      console.log(this.state);
-    });
-  }
-
-  render() {
-    return (
-      <form>
-        app2 <input name="app2" type="checkbox" value={this.state.app2} onChange={this.handleChange} />        
-        app3 <input name="app3" type="checkbox" value={this.state.app3} onChange={this.handleChange} />        
-      </form>
-    );
-  }
+  return (
+    <form>
+      app2 <input name="app2" type="checkbox" value={plugins.app2} onChange={handleChange} />        
+      app3 <input name="app3" type="checkbox" value={plugins.app3} onChange={handleChange} />        
+    </form>
+  );  
 }

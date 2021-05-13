b06c901b2600a3addfe43e6888a5ce37f05f620d
Giuseppe de Santis <gdesantis@gmail.com>
2021-05-13 17:07:05 +0200

    refactor the "extend" feature to a reusable component

--- shell/src/Box.js
+++ shell/src/Box.js
@@ -1,45 +1,15 @@
 import React from "react";
 import DefaultBox from "./DefaultBox";
+import Extend from "./Extend";
 
 const ExtendedBox = React.lazy(() => import("extensions/ExtendedBox"));
-/*
-this is implemented as class component in order to use the error boundaries react feature. 
- You can achieve the same result without class components using https://www.npmjs.com/package/react-error-boundary
-*/
-class Box extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = { hasError: false };
-  }
 
-  static getDerivedStateFromError() {
-    return { hasError: true };
-  }
-
-  componentDidCatch() {}
-
-  render() {
-    if (this.state.hasError) {
-      return (
-        <React.Suspense fallback={<div>Loading fallback header</div>}>
-          <DefaultBox {...this.props} />
-        </React.Suspense>
-      );
-    }
-
-    return (
-      <React.Suspense fallback={<div>loading</div>}>
-        <ExtendedBox {...this.props} />
-      </React.Suspense>
-    );
-  }
+export default function Box( { name }) {
+  return (
+    <Extend
+      defaultComponent={DefaultBox}
+      extendedComponent={ExtendedBox}
+      name={name}
+    />
+  );
 }
-
-const App = () => (
-  <div>
-    <HeaderWrapper />
-    <div>Hi there, I'm React from React.</div>
-  </div>
-);
-
-export default Box;
---
+++ shell/src/Extend.js
@@ -0,0 +1,33 @@
+import React from "react";
+
+class Extend extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = { hasError: false };
+  }
+
+  static getDerivedStateFromError() {
+    return { hasError: true };
+  }
+
+  componentDidCatch() {}
+
+  render() {
+    const { defaultComponent: DefaultComponent, extendedComponent: ExtendedComponent, ...rest } = this.props;
+    if (this.state.hasError) {
+      return (
+        <React.Suspense fallback={<div>Loading default component...</div>}>
+          <DefaultComponent {...rest} />
+        </React.Suspense>
+      );
+    }
+
+    return (
+      <React.Suspense fallback={<div>Loading extended component...</div>}>
+        <ExtendedComponent {...rest} />
+      </React.Suspense>
+    );
+  }
+}
+
+export default Extend;
